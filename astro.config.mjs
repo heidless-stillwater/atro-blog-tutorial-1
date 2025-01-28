5d8fa911ab226a40feec80eb76a6be1f4ba0fb87
heidless <lockhart.r@gmail.com>
2025-01-28 14:06:15 +0000

    latest

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+import icon from "astro-icon";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [icon()]
+});
